2b256bf9002505e1472dfb4635d3e860e21b7222
Mads H. Danquah <mads@danquah.dk>
2016-04-08 15:06:40 +0200

    Fixing permission and stdout issue

--- perftest.sh
+++ perftest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Downloading Drupal 7"
-drush dl -q -y --drupal-project-rename=storage drupal-7
-cd storage
+drush dl -q -y --drupal-project-rename=storage/drupal drupal-7
+cd storage/drupal
 echo "Performing site install"
 time drush si standard -y --db-url=sqlite://sites/default/files/.ht.sqlite
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 echo "Testing local storage"
-docker run --rm danquah/drupal-perf-test
+docker run -ti --rm danquah/drupal-perf-test
 echo
 echo "Testing volume mount"
-docker run --rm -v ~/storage:/storage danquah/drupal-perf-test
+docker run -ti --rm -v ~/storage:/storage danquah/drupal-perf-test
