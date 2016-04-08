f6e1aeba92b6c2cd38508cd416e66a489a6782ac
Mads H. Danquah <mads@danquah.dk>
2016-04-08 14:42:50 +0200

    Output formatting

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+echo "Testing local storage"
 docker run --rm danquah/drupal-perf-test
+echo
+echo "Testing volume mount"
 docker run --rm -v ~/storage:/storage danquah/drupal-perf-test
